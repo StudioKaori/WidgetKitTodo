439dd9ed81d904d26ec56e66e6d2243571faca34
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-08 20:13:19 +0200

    Initial Commit

---
+++ WidgetKitTodo/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WidgetKitTodo
+//
+//  Created by Kaori Persson on 2022-10-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WidgetKitTodo/WidgetKitTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetKitTodoApp.swift
+//  WidgetKitTodo
+//
+//  Created by Kaori Persson on 2022-10-08.
+//
+
+import SwiftUI
+
+@main
+struct WidgetKitTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
