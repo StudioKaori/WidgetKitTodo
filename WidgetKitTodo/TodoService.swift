95b4c6757909562a48c21ebefec02bc0f85b019e
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-08 20:24:39 +0200

    Update: fetch()

--- WidgetKitTodo/TodoService.swift
+++ WidgetKitTodo/TodoService.swift
@@ -13,6 +13,14 @@ final class TodoService {
   private let baseURL = "https://jsonplaceholder.typicode.com/"
   
   private func fetch<T: Decodable>(from endpoint: String) async throws -> T {
+    let urlString = baseURL + endpoint
+    guard let url = URL(string: urlString) else {
+      throw URLError(.badURL)
+    }
     
+    let (data, _) = try await URLSession.shared.data(from: url)
+    
+    let decoder = JSONDecoder()
+    let result = try decoder.decode(T.self, from: data)
   }
 }
