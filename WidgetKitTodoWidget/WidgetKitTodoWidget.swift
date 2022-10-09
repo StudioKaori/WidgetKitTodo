304af52b4e61f18c04aee8ae8d26a29edd5d9733
StudioKaori <67104053+StudioKaori@users.noreply.github.com>
2022-10-09 09:44:29 +0200

    Refactor: change the name to WidgetView

--- WidgetKitTodoWidget/WidgetKitTodoWidget.swift
+++ WidgetKitTodoWidget/WidgetKitTodoWidget.swift
@@ -8,21 +8,13 @@
 import WidgetKit
 import SwiftUI
 
-struct WidgetKitTodoWidgetEntryView : View {
-    var entry: Provider.Entry
-
-    var body: some View {
-        Text(entry.date, style: .time)
-    }
-}
-
 @main
 struct WidgetKitTodoWidget: Widget {
     let kind: String = "WidgetKitTodoWidget"
 
     var body: some WidgetConfiguration {
         StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            WidgetKitTodoWidgetEntryView(entry: entry)
+            WidgetView(entry: entry)
         }
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
@@ -31,7 +23,7 @@ struct WidgetKitTodoWidget: Widget {
 
 struct WidgetKitTodoWidget_Previews: PreviewProvider {
     static var previews: some View {
-        WidgetKitTodoWidgetEntryView(entry: SimpleEntry(date: Date()))
+        WidgetView(entry: SimpleEntry(date: Date()))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
---
+++ WidgetKitTodoWidget/WidgetView.swift
@@ -0,0 +1,17 @@
+//
+//  WidgetKitTodoWidgetEntryView.swift
+//  WidgetKitTodoWidgetExtension
+//
+//  Created by Kaori Persson on 2022-10-09.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct WidgetView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        Text(entry.date, style: .time)
+    }
+}
